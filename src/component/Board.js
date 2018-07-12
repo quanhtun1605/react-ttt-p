66ef657c7216d737af42bf8c7ad949152861a69b
Tranvanphuong <tranphuongtna1@gmail.com>
2018-07-12 09:10:39 +0700

    xem nop tut

---
+++ src/component/Board.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Square from './Square';
+
+class Board extends React.Component{
+  renderSquare(i){
+    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>
+  }
+
+  render(){
+    const matrixSize = Math.sqrt(this.props.squares.length);
+    const rows = Array(matrixSize).fill(null);
+    const cols = rows;
+    const board = rows.map((row, i) => {
+      const squares = cols.map((col, j) => {
+        const squareKey = i * matrixSize + j;
+        return <span key={squareKey}>{this.renderSquare(squareKey)}</span>;
+      });
+      return <div className="board-row" key={i}>{squares}</div>
+    });
+    return(
+      <div>
+
+        <div>Board</div>
+        <div>{board}</div>
+      </div>
+    );
+  }
+}
+
+export default Board;
